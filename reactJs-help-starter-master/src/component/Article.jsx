fa7bad96664c811dd5840bc0946fd5cb01e5ac1a
MisterBulle <101103057+MisterBulle@users.noreply.github.com>
2022-12-12 09:09:28 +0100

    SaveArticles Implementé / Not Working

--- reactJs-help-starter-master/src/component/Article.jsx
+++ reactJs-help-starter-master/src/component/Article.jsx
@@ -4,10 +4,11 @@ import { usePostStore } from '../store/postStore';
 
 import {Patch_Request_Articles,DELETE_Article_REQUEST,} from '../component/Articles_Request';
 
+//Recup token
 var token_sessionstorage = sessionStorage.getItem("letoken")
-console.log("TEST")
 console.log(token_sessionstorage)
 
+var recupDataID;
 
 function Article() {
 	let { id } = useParams();
@@ -19,13 +20,36 @@ function Article() {
 		setPost(posts.find((item) => Number(item.id) === Number(id)));
 	}, [id, posts]);
 
+	const HClick_Save_Articles = async () => {
+        try {
+                const response = await fetch('http://127.0.0.1:8000/api/save_blogs',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        article_id: recupDataID,
+                    }),
+                    headers: { 'Content-Type': 'application/json', Accept: 'application/json',
+                    },
+                });
+				console.log("ID enregistré", recupDataID)
+                //Erreur de fetch
+                if (!response.ok) {
+                    throw new Error(`Error! status: ${response.status}`);
+                }
+                const result = await response.json();
+            } 
+                catch { console.log("ERROR"); }
+        };
+
+
 	return (
 		<div>
 			{post && (
 				<>
-					<h2>{post.id}</h2>
+				<script> {recupDataID = post.id} </script> {/*Recup DATA*/}
 					<h2>{post.title}</h2>
 					<p>{post.body}</p>
+					<button onClick={HClick_Save_Articles}>Enregistrer son article </button>
 					<Link to={`/`}>retour à la liste</Link>
 				</>
 			)}
@@ -35,5 +59,6 @@ function Article() {
 		</div>
 	);
 }
+
 
 export default Article;
